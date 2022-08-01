2e1ed0785550a2ae5d5e06126220c90923019ede
liang.cheng <glimmer.treasure@gmail.com>
2022-08-01 22:56:27 +0800

    feat: 完成组件库打包的基础配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'clown-ui',
+      fileName: (format) => `clown-ui.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue'], // 将vue排除，所有导入vue的地方不会把vue源码导入而是转变成require('vue')的形式
+      output: {
+        globals: {
+          vue: 'Vue' // 将全局变量vue改为Vue
+        }
+      }
+    }
+  },
 })
